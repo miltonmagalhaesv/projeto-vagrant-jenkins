ee651c8ead3b25fd33ced6adb952a4f9e0b41a0a
miltonmagalhaesv <miltonmagalhaesv@gmail.com>
2026-09-03 16:45:36 -0300

    fix: atualiza provisionamento do Jenkins

--- vagrant/scripts/setup-jenkins.sh
+++ vagrant/scripts/setup-jenkins.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+sudo rm -f /etc/apt/sources.list.d/jenkins.list
 sudo apt-get update
-sudo apt-get install -y fontconfig openjdk-17-jre git curl
+sudo apt-get install -y fontconfig openjdk-21-jre git curl
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key \
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key \
   | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
